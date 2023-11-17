1e90a61ca8aa5aa1f1427985bd1bdca916202f7e
Sanjay <85150488+sanjay103108@users.noreply.github.com>
2023-11-17 12:50:36 +0530

    Add files via upload

---
+++ HomeComponent.js
@@ -0,0 +1,140 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './styles.css'; 
+import Pizza from "./Images/PIZZAPIC.jpg";
+import Burger from "./Images/BURGERPIC.jpg";
+
+function HomeComponent() {
+  return (
+    <div>
+      <div id="react-buttons"></div>
+      <script src="./build/static/js/main.js"></script>
+      <section className="menu">
+        <div className="nav">
+          <div className="logo">
+            <h1>
+              Food<b>io</b>
+            </h1>
+          </div>
+          <ul>
+            <li>
+              <a className="active" href="#">
+                Home
+              </a>
+            </li>
+            <li>
+              <Link to="/menu">Menu</Link>
+            </li>
+            <li>
+              <a href="services">Service</a>
+            </li>
+            <li>
+              <a href="about">About US</a>
+            </li>
+          </ul>
+          <div></div>
+        </div>
+      </section>
+      <section className="grid">
+        <div className="content">
+          <div className="content-left">
+            <div className="info">
+              <h2>Order Your Favorite<br />Food Anytime</h2>
+              <p>
+                Hey, Our delicious food is waiting for you,<br />
+                We are always near you with our fresh food{' '}
+              </p>
+            </div>
+            <Link to="/menu" className="exploreFoodButton">Explore Food</Link>
+          </div>
+          <div className="content-right">
+            <img src={Pizza} alt="" />
+          </div>
+        </div>
+      </section>
+      <section className="category">
+        <div className="list-items">
+          <h3>Popular Dishes</h3>
+          <div className="card-list">
+            {/* Chicken Burger Card */}
+            <div className="card">
+              <img src={Burger} alt="" />
+              <div className="food-title">
+                <h1>Chicken Burger</h1>
+              </div>
+              <div className="desc-food">
+                <p>
+                  Juicy grilled chicken patty, topped with fresh lettuce, ripe
+                  tomatoes, and our special secret sauce, all served in a
+                  toasted sesame bun
+                </p>
+              </div>
+              <div className="price">
+                <span>₹500.00</span>
+                <span>
+                  <i className="bx bxs-plus-circle"></i>
+                </span>
+              </div>
+            </div>
+
+            {/* Lasagna Card */}
+            <div className="card">
+              <div className="food-title">
+                <h1>Lasagna</h1>
+              </div>
+              <div className="desc-food">
+                <p>
+                  Layers of pasta, rich meat sauce, creamy béchamel, and melted cheese baked to perfection.
+                </p>
+              </div>
+              <div className="price">
+                <span>₹550.00</span>
+                <span>
+                  <i className="bx bxs-plus-circle"></i>
+                </span>
+              </div>
+            </div>
+
+            {/* Loaded Nachos Card */}
+            <div className="card">
+              <div className="food-title">
+                <h1>Loaded Nachos</h1>
+              </div>
+              <div className="desc-food">
+                <p>
+                  Crispy tortilla chips topped with melted cheese, seasoned beef, tomatoes, and jalapeños.
+                </p>
+              </div>
+              <div className="price">
+                <span>₹450.00</span>
+                <span>
+                  <i className="bx bxs-plus-circle"></i>
+                </span>
+              </div>
+            </div>
+
+            {/* Tacos Card */}
+            <div className="card">
+              <div className="food-title">
+                <h1>Tacos</h1>
+              </div>
+              <div className="desc-food">
+                <p>
+                  Soft tortillas filled with seasoned meat, lettuce, cheese, and your favorite toppings.
+                </p>
+              </div>
+              <div className="price">
+                <span>₹400.00</span>
+                <span>
+                  <i className="bx bxs-plus-circle"></i>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default HomeComponent;
---
+++ ServicesComponent.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link component
+import './styles.css'; // Import your CSS file
+
+function ServicesComponent() {
+  return (
+    <div>
+      <section className="menu">
+        <div className="nav">
+          <div className="logo">
+            <h1>
+              Food<b>io</b>
+            </h1>
+          </div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/menu">Menu</Link>
+            </li>
+            <li>
+              <Link to="/services" className="active">
+                Service
+              </Link>
+            </li>
+            <li>
+              <Link to="/about">About US</Link>
+            </li>
+          </ul>
+        </div>
+      </section>
+      <section className="services">
+        <h2>Our Services</h2>
+        <div className="services-item">
+          <h3>Online Food Ordering</h3>
+          <p>
+            Order your favorite dishes from a wide variety of restaurants and
+            cuisines through our user-friendly platform.
+          </p>
+          
+        </div>
+        <div className="services-item">
+          <h3>Delivery & Takeout</h3>
+          <p>
+            We offer fast and reliable delivery services, and if you prefer,
+            you can opt for convenient takeout options as well.
+          </p>
+        </div>
+
+        <div className="services-item">
+          <h3>Easy Payment</h3>
+          <p>
+            Enjoy hassle-free payments with various options, including credit
+            cards, digital wallets, and cash on delivery.
+          </p>
+        </div>
+
+        <div className="services-item">
+          <h3>Diverse Cuisines</h3>
+          <p>
+            Explore a world of flavors with a diverse range of cuisines, from
+            Italian to Mexican and everything in between.
+          </p>
+        </div>
+
+        <div className="services-item">
+          <h3>Special Features</h3>
+          <p>
+            Discover our special features, such as dietary options, family meal
+            deals, and catering services for your events.
+          </p>
+        </div>
+
+        <div className="services-item">
+          <h3>Customer Support</h3>
+          <p>
+            Our customer support team is available to assist you with any
+            questions or issues. Contact us 24/7.
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default ServicesComponent;
